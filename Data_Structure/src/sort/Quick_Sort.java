2c3a6d3f40e41981ec3f64a055a417e150ca1796
Asterisk-C <hsinhaochen334@gmail.com>
2021-10-18 23:22:34 +0100

    Quick_Sort added

---
+++ Data_Structure/src/sort/Quick_Sort.java
@@ -0,0 +1,56 @@
+package sort;
+
+public class Quick_Sort 
+{
+	public static void quickSort(int[] arr, int low, int high)
+	{
+		int pivot = (low + high) / 2;
+		int i = low;
+		int j = high;
+		int tmp;
+		
+		while(i <= j)
+		{
+			while(arr[i] < arr[pivot])
+			{
+				i++;
+			}
+			while(arr[j] > arr[pivot])
+			{
+				j--;
+			}
+			
+			if(i <= j)
+			{
+				if(i != j)
+				{
+					tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;		
+				}
+				i++;
+				j--;
+			}
+					
+		}
+			
+		if(low < j)
+			quickSort(arr, low, j);
+		if(high > i)
+			quickSort(arr, i, high);
+    
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int[] arr = new int[]{5, 9, 1, 9, 5, 3, 7, 6, 1};
+		quickSort(arr, 0, arr.length - 1);
+		for(int i: arr)
+		{
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
