23e62cd4060b0b41237c578fac25548832aadb60
Asterisk-C <hsinhaochen334@gmail.com>
2021-12-22 13:09:19 +0000

    Back up.

--- Java_Fundamental/src/file_IO/TestFile.java
+++ Java_Fundamental/src/file_IO/TestFile.java
@@ -10,9 +10,8 @@ import java.io.IOException;
 public class TestFile 
 {
 	public static void main(String[] args)
-	{
-		
-//		//Read NBA.txt
+	{		
+//		// Read NBA.txt
 //		try
 //		{
 //			File f = new File("E:/Github Projects/University-Project/Java Fundamental/NBA.txt");
@@ -30,69 +29,69 @@ public class TestFile
 //		{
 //			e.printStackTrace();
 //		}
+//		
+//		// Write information into CBA.txt
+//		try
+//		{
+//			File f = new File("E:/Github Projects/University-Project/Java Fundamental/CBA.txt");
+//			//If the folder doesn't exist, build it.
+//			File dir = f.getParentFile();
+//			if(!dir.exists())
+//				dir.mkdirs();
+//			
+//			FileOutputStream fs = new FileOutputStream(f);
+//			byte[] data = {67, 66, 65};
+//			fs.write(data);
+//			fs.close();
+//		}
+//		catch(IOException e)
+//		{
+//			e.printStackTrace();
+//		}
+		       
+		// Basic command
+		File f = new File("E:/Github Projects/University-Project/Java Fundamental/CBA.txt");
 		
-		// Write information into CBA.txt
-		try
-		{
-			File f = new File("E:/Github Projects/University-Project/Java Fundamental/CBA.txt");
-			//If the folder doesn't exist, build it.
-			File dir = f.getParentFile();
-			if(!dir.exists())
-				dir.mkdirs();
-			
-			FileOutputStream fs = new FileOutputStream(f);
-			byte[] data = {67, 66, 65};
-			fs.write(data);
-			fs.close();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
+		System.out.println("当前文件是：" +f);
 		
+        //文件是否存在
+        System.out.println("判断是否存在："+f.exists());
+         
+        //是否是文件夹
+        System.out.println("判断是否是文件夹："+f.isDirectory());
+          
+        //是否是文件（非文件夹）
+        System.out.println("判断是否是文件："+f.isFile());
+          
+        //文件长度
+        System.out.println("获取文件的长度："+f.length());
+          
+        //文件最后修改时间
+        long time = f.lastModified();
+        Date d = new Date(time);
+        System.out.println("获取文件的最后修改时间："+d);
         
-//		// Basic command
-//		
-//		System.out.println("当前文件是：" +f);
-//		
-//        //文件是否存在
-//        System.out.println("判断是否存在："+f.exists());
-//         
-//        //是否是文件夹
-//        System.out.println("判断是否是文件夹："+f.isDirectory());
-//          
-//        //是否是文件（非文件夹）
-//        System.out.println("判断是否是文件："+f.isFile());
-//          
-//        //文件长度
-//        System.out.println("获取文件的长度："+f.length());
-//          
-//        //文件最后修改时间
-//        long time = f.lastModified();
-//        Date d = new Date(time);
-//        System.out.println("获取文件的最后修改时间："+d);
-//        
 //        //设置文件修改时间为1970.1.1 08:00:00
 //        f.setLastModified(0);
-//          
+          
 //        //文件重命名
-//        File f2 =new File("E:/Github Projects/University-Project/Java Fundamental/CBA.exe");
+//        File f2 =new File("E:/Github Projects/University-Project/Java Fundamental/CUBA.exe");
 //        f.renameTo(f2);
-//        System.out.println("把NBA.exe改名成了CBA.exe");      
-//        System.out.println("注意： 需要在path确实存在一个NBA.exe,\r\n才可以看到对应的文件长度、修改时间等信息");
-//        
-//        // 以字符串数组的形式，返回当前文件夹下的所有文件（不包含子文件及子文件夹）
-//        f.list();
-//  
-//        // 以文件数组的形式，返回当前文件夹下的所有文件（不包含子文件及子文件夹）
-//        File[]fs= f.listFiles();
-//  
-//        // 以字符串形式返回获取所在文件夹
-//        f.getParent();
-//  
-//        // 以文件形式返回获取所在文件夹
-//        f.getParentFile();
-//        
+//        System.out.println("把CUCBA.exe改名成了CUBA.exe");      
+//        System.out.println("注意： 需要在path确实存在一个CUCBA.exe,\r\n才可以看到对应的文件长度、修改时间等信息");
+        
+        // 以字符串数组的形式，返回当前文件夹下的所有文件（不包含子文件及子文件夹）
+        f.list();
+  
+        // 以文件数组的形式，返回当前文件夹下的所有文件（不包含子文件及子文件夹）
+        File[]fs= f.listFiles();
+  
+        // 以字符串形式返回获取所在文件夹
+        f.getParent();
+  
+        // 以文件形式返回获取所在文件夹
+        f.getParentFile();
+        
 //        // 创建文件夹，如果父文件夹skin不存在，创建就无效
 //        f.mkdir();
 //  
@@ -104,15 +103,16 @@ public class TestFile
 //        
 //        // 所以创建一个空文件之前，通常都会创建父目录
 //        f.getParentFile().mkdirs();
-//  
-//        // 列出所有的盘符c: d: e: 等等
-//        f.listRoots();
-//  
+  
+        // 列出所有的盘符c: d: e: 等等
+        f.listRoots();
+  
 //        // 刪除文件
 //        f.delete();
 //  
 //        // JVM结束的时候，刪除文件，常用于临时文件的删除
 //        f.deleteOnExit();
+
         
 	}
 }
