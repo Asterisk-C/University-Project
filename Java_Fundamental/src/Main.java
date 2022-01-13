105f3594625898e730c533981233a8a1aa0458a1
Asterisk-C <hsinhaochen334@gmail.com>
2022-01-13 00:29:47 +0000

    Exercise done today.

--- Java_Fundamental/src/Main.java
+++ Java_Fundamental/src/Main.java
@@ -6,6 +6,8 @@ public class Main
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
+		String s = "I wanna fuck with you.";
+		System.out.println(s);
 /*
 		flag:
 		for(int n = 0; n < 100; n++)
@@ -59,17 +61,17 @@ public class Main
 		System.arraycopy(b, 0, c, a.length, b.length);
 		System.out.println(Arrays.toString(c));
 */
-		int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
-		 
-        // copyOfRange(int[] original, int from, int to)
-        // 第一个参数表示源数组
-        // 第二个参数表示开始位置(取得到)
-        // 第三个参数表示结束位置(取不到)
-        int[] b = Arrays.copyOfRange(a, 0, 3);
- 
-        for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i] + " ");
-        }
+//		int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+//		 
+//        // copyOfRange(int[] original, int                                                                                                                                                                                                                                                                                                                          from, int to)
+//        // 第一个参数表示源数组
+//        // 第二个参数表示开始位置(取得到)
+//        // 第三个参数表示结束位置(取不到)
+//        int[] b = Arrays.copyOfRange(a, 0, 3);
+// 
+//        for (int i = 0; i < b.length; i++) {
+//            System.out.print(b[i] + " ");
+//        }
 	}
 
 }
---
+++ Java_Fundamental/src/file_IO/TestBufferedReader.java
@@ -0,0 +1,38 @@
+package file_IO;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestBufferedReader 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		File f = new File("E:/Java/University-Project/Java_Fundamental/NBA.txt");
+		try
+		(
+				FileReader fr = new FileReader(f);
+				BufferedReader br = new BufferedReader(fr);
+		)
+		{
+			while(true)
+			{
+				String line = br.readLine();
+				if(line == null)
+				{
+					break;
+				}
+				System.out.println(line);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
